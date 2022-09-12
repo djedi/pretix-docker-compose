3d1fc31047bd37a0b2590e86fb6a9840151f7683
Dustin Davis <dustin.davis@edgecast.com>
2022-09-12 16:04:13 -0600

    update setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Replace example.com with your domain
+read -p "Enter your domain name (example.com): " domain
+sed -i "s/exmple.com/$domain/g" pretix.cfg
+sed -i "s/exmple.com/$domain/g" docker-compose.yml
+
 # install docker
 apt install docker.io docker-compose
 
@@ -7,17 +12,13 @@ apt install docker.io docker-compose
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
 
-# Replace example.com with your domain
-read -p "Enter your domain name (example.com): " domain
-sed -i "s/exmple.com/$domain/g" pretix.cfg
-
 # Create folder structure for pretix
-mkdir /var/pretix-data
+mkdir -p /var/pretix-data
 chown -R 15371:15371 /var/pretix-data
-mkdir /var/pgdata
+mkdir -p /var/pgdata
 
 # Config directory and file for pretix
-mkdir /etc/pretix
+mkdir -p /etc/pretix
 cp ./pretix.cfg /etc/pretix/pretix.cfg
 chown -R 15371:15371 /etc/pretix
 chmod 0700 /etc/pretix/pretix.cfg
