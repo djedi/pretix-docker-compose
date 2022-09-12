709d2c08a066cb24c52cb1e710e5aaf526e24e4f
Dustin Davis <dustin.davis@edgecast.com>
2022-09-12 16:00:02 -0600

    install docker-compose

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 # install docker
-apt install docker.io
+apt install docker.io docker-compose
 
 # install portainer
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest
+
+# Replace example.com with your domain
+read -p "Enter your domain name (example.com): " domain
+sed -i "s/exmple.com/$domain/g" pretix.cfg
 
 # Create folder structure for pretix
 mkdir /var/pretix-data
@@ -19,10 +23,12 @@ chown -R 15371:15371 /etc/pretix
 chmod 0700 /etc/pretix/pretix.cfg
 
 # Run docker compose file
-cp ./pretix.service /etc/systemd/system/pretix.service
-systemctl daemon-reload
-systemctl enable pretix
-systemctl start pretix
+# cp ./pretix.service /etc/systemd/system/pretix.service
+# systemctl daemon-reload
+# systemctl enable pretix
+# systemctl start pretix
+docker-compose up
+
 
 # Create cron job
 (crontab -l 2>/dev/null; echo "15,45 * * * * /usr/bin/docker exec pretix.service pretix cron") | crontab -
